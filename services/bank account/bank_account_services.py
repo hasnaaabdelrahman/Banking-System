ec95b402ae9e2d91c716eb22ac47f7fc00b31c95
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-07-01 11:50:11 +0300

    feat: implement account services

---
+++ services/bank account/bank_account_services.py
@@ -0,0 +1,13 @@
+from models.bank_account import BankAccount
+from exceptions.bank_account_exceptions import BankAccountNotFound , BankAccountAlreadyExists
+
+
+class BankAccountService:
+    def __init__(self , bank_repository):
+        self.bank_repository = bank_repository
+
+    def create_bank_account(self , bank_account:BankAccount) -> BankAccount:
+        if self.bank_repository.get_by_id(bank_account.id) or self.bank_repository.get_by_user_id(bank_account.user_id):
+            raise BankAccountAlreadyExists("Bank account already exists")
+        self.bank_repository.create(bank_account)
+        return bank_account
