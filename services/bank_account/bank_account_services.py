824f4e86c4860499dc6d6fae00c893c498416f30
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-07-01 18:24:09 +0300

    feat: implement missing services

--- services/bank_account/bank_account_services.py
+++ services/bank_account/bank_account_services.py
@@ -11,3 +11,27 @@ class BankAccountService:
             raise BankAccountAlreadyExists("Bank account already exists")
         self.bank_repository.create(bank_account)
         return bank_account
+
+    def get_by_id(self , id:str) -> BankAccount:
+        account = self.bank_repository.get_by_id(id)
+        if account is None:
+            raise BankAccountNotFound("Bank account not found")
+        return self.bank_repository.get_by_id(id)
+
+    def get_by_user_id(self , user_id:str) -> BankAccount:
+        account = self.bank_repository.get_by_user_id(user_id)
+        if account is None:
+            raise BankAccountNotFound("Bank account not found")
+        return self.bank_repository.get_by_user_id(user_id)
+
+    def delete_by_id(self , id:str) -> bool:
+        account = self.bank_repository.get_by_id(id)
+        if account is None:
+            raise BankAccountNotFound("Bank account not found")
+        return self.bank_repository.delete_by_id(id)
+
+    def update_by_id(self , id:str) -> bool:
+        account = self.bank_repository.get_by_id(id)
+        if account is None:
+            raise BankAccountNotFound("Bank account not found")
+        return self.bank_repository.update_by_id(id)
