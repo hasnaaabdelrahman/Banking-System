257b83379d0e0271b93a27a9058803fcf68039c3
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-07-01 18:07:31 +0300

    test: implement unit tests for custom exception

--- tests/test_transaction_services.py
+++ tests/test_transaction_services.py
@@ -2,6 +2,7 @@ import unittest
 import datetime
 
 from common.transaction_type import TransactionType
+from exceptions.transaction_exceptions import TransactionNotFound
 from services.transaction.transaction_services import TransactionService
 
 
@@ -21,14 +22,71 @@ class TestTransactionService(unittest.TestCase):
         transaction = self.transaction_service.create_transaction(self.transaction)
         self.assertEqual(self.transaction , transaction)
 
+    def test_get_all_transactions(self):
+        transactions = self.transaction_service.get_all_transactions()
+        self.assertNotEqual(len(transactions), 2)
+
+    def test_get_transaction_by_id(self):
+        self.transaction_repository.transaction = self.transaction
+        transaction = self.transaction_service.get_transaction_by_id(self.transaction.id)
+        self.assertEqual(self.transaction , transaction)
+
+    def test_get_transaction_by_account_id(self):
+        self.transaction_repository.transaction = self.transaction
+        transactions = self.transaction_service.get_transaction_by_account_id(self.transaction.account_id)
+        self.assertEqual(self.transaction , transactions)
+
+    def test_update_transaction(self):
+        self.transaction_repository.transaction = self.transaction
+        transaction = self.transaction_service.update_transaction(self.transaction)
+        self.assertEqual(self.transaction , transaction)
+
+    def test_delete_transaction(self):
+        self.transaction_repository.transaction = self.transaction
+        transaction = self.transaction_service.delete_transaction(self.transaction)
+        self.assertEqual(self.transaction , transaction)
+
+    def test_transaction_not_found_will_raise_exception(self):
+        with self.assertRaises(TransactionNotFound):
+            self.transaction_service.get_transaction_by_id(self.transaction.id)
+
+    def test_transaction_not_found_by_account_id_will_raise_exception(self):
+        with self.assertRaises(TransactionNotFound):
+            self.transaction_service.get_transaction_by_account_id(self.transaction.account_id)
+
+
 class TransactionRepositorySpy:
     def __init__(self):
         self.transaction = None
+        self.transactions_list =[]
 
     def create_transaction(self , transaction):
-        self.transaction = transaction
+        if transaction.amount < 0:
+            return None
         return transaction
 
+    def get_all_transactions(self):
+        return self.transactions_list
+
+    def get_transaction_by_id(self , id):
+        if self.transaction and self.transaction.id == id:
+            return self.transaction
+        return None
+
+    def get_transaction_by_account_id(self , account_id):
+        if self.transaction and self.transaction.account_id == account_id:
+            return self.transaction
+        return None
+
+    def update_transaction(self , transaction):
+        if self.transaction:
+            return self.transaction
+        return transaction
+
+    def delete_transaction(self , transaction):
+        if self.transaction:
+            return self.transaction
+        return None
 
 class TransactionDouble:
     def __init__(self, id = '12' ,
