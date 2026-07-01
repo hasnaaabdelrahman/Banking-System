7a1c3edbc30347a6b47764206170fa838e6761d6
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-07-01 11:50:39 +0300

    test: setup tests for bank

---
+++ tests/test_bank_account_services.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestBankAccountService(unittest.TestCase):
+    pass
+
+class BankAccountRepositorySpy:
+    pass
+
+class BankAccountDouble:
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
