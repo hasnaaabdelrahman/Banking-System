9b164aa694b15e8e40b95c0b119f50eb7a7c00b1
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-07-01 18:24:44 +0300

    test: implement missing unit tests

--- tests/test_bank_account_services.py
+++ tests/test_bank_account_services.py
@@ -28,6 +28,16 @@ class TestBankAccountService(unittest.TestCase):
         with self.assertRaises(BankAccountAlreadyExists):
             self.bank_account_services.create_bank_account(self.bank_account)
 
+    def test_update_bank_account(self):
+        self.bank_account_repository.bank_account = self.bank_account
+        account = self.bank_account_services.update_by_id(self.bank_account.id)
+        self.assertEqual(self.bank_account , account)
+
+    def test_delete_bank_account(self):
+        self.bank_account_repository.bank_account = self.bank_account
+        account = self.bank_account_services.delete_by_id(self.bank_account.id)
+        self.assertEqual(self.bank_account , account)
+
 class BankAccountRepositorySpy:
     def __init__(self):
         self.bank_account = None
@@ -44,6 +54,16 @@ class BankAccountRepositorySpy:
     def create(self, bank_account):
         self.bank_account = bank_account
         return self.bank_account
+
+    def update_by_id(self , id):
+        if self.bank_account and self.bank_account.id == id:
+            return self.bank_account
+        return None
+
+    def delete_by_id(self , id):
+        if self.bank_account and self.bank_account.id == id:
+            return self.bank_account
+        return None
 
 class BankAccountDouble:
     def __init__(self ,
