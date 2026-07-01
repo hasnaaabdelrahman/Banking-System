4a038259a90092b8198ff1f2a72416ec60db65e8
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-07-01 07:42:41 +0300

    feat: implement user services

--- services/user/user_services.py
+++ services/user/user_services.py
@@ -1,2 +1,23 @@
-import models.user as user
-import datetime
+from models.user import User
+from utils import password
+from exceptions.user_exceptions import UserAlreadyExists , UserNotFound , EmailAlreadyExists , InvalidCredentials
+
+class UserService:
+    def __init__(self , user_repository):
+        self.user_repository = user_repository
+
+    def register(self,user : User) -> User:
+        if self.user_repository.get_by_username(user.username):
+            raise UserAlreadyExists('user already exists')
+        if self.user_repository.get_by_email(user.email):
+            raise EmailAlreadyExists('email already exists')
+        user.password = password.hash_password(user.password)
+        return self.user_repository.create(user)
+
+    def login(self , username:str , user_password:str) -> User:
+        user = self.user_repository.get_by_username(username)
+        if not user:
+            raise UserNotFound('user does not exist.')
+        if not  password.check_password(user_password , user.password):
+            raise InvalidCredentials('Invalid username or password.')
+        return user
