15755b8c0850aef77aaa6c6c98547c710b800443
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-07-04 14:40:11 +0300

    testing evn

--- features/environment.py
+++ features/environment.py
@@ -1,10 +1,12 @@
 from services.user.user_services import UserService
 from repositories.user_repository import UserRepository
+from models.user import User
 from DB.db import session
 
-def before_all(context):
-    print(">>> before_all is running")
+def before_scenario(context, scenario):
     db = session()
-    user_repository = UserRepository(db)
-    context.user_service = UserService(user_repository)
 
+    db.query(User).delete()
+    db.commit()
+
+    context.user_service = UserService(UserRepository(db))
