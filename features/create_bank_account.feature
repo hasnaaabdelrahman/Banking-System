0ae2f01e0f72b867c2c91d6193c48a39eaeafee9
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-07-04 18:50:07 +0300

    test: implement bad scenario tests

--- features/create_bank_account.feature
+++ features/create_bank_account.feature
@@ -4,3 +4,9 @@ Feature: Create Bank Account
       Given the user wants to create a bank account
       When  the user is registered and logged in
       Then  the bank account is created successfully
+
+  Scenario: Bank Account is not created
+    Given the user is registered and logged in and wants to create a bank account
+    When the user is already has an bank account
+    Then the bank account is not created
+
--- features/steps/create_bank_account_steps.py
+++ features/steps/create_bank_account_steps.py
@@ -2,6 +2,7 @@
 from behave import given, when, then
 
 from common.account_type import AccountType
+from exceptions.bank_account_exceptions import BankAccountAlreadyExists
 from models import User
 from models.bank_account import BankAccount
 
@@ -16,6 +17,7 @@ def step_impl(context):
         user_id='1'
     )
     context.user = User(
+        id = '1',
         username="user_13",
         password="password@12",
         email="user1@example.com",
@@ -28,9 +30,44 @@ def step_impl(context):
 @when("the user is registered and logged in")
 def step_impl(context):
     context.user = context.user_service.register(context.user)
-    context.result = context.user_service.login("user_13", "password@12")
-
+    context.logged = context.user_service.login("user_13", "password@12")
+    context.result = context.bank_account_service.create_bank_account(context.bank_account)
 
 @then("the bank account is created successfully")
 def step_impl(context):
     assert context.result is not None
+
+@given("the user is registered and logged in and wants to create a bank account")
+def step_impl(context):
+    context.bank_account = BankAccount(
+        id='123',
+        account_number='123456789',
+        account_type=AccountType.SAVINGS,
+        balance=1000,
+        is_active=True,
+        user_id='2'
+    )
+    context.user = User(
+        username="user_23",
+        password="password@12",
+        email="user2@example.com",
+        first_name="fuser",
+        last_name="luser",
+        age=22,
+        img_url="https://example.com",
+        bank_account=[]
+    )
+    context.user = context.user_service.register(context.user)
+    context.logged = context.user_service.login("user_23", "password@12")
+    context.result = context.bank_account_service.create_bank_account(context.bank_account)
+
+@when("the user is already has an bank account")
+def step_impl(context):
+    try:
+        context.result = context.bank_account_service.create_bank_account(context.bank_account)
+    except (BankAccountAlreadyExists) as e:
+        context.exception = e
+
+@then("the bank account is not created")
+def step_impl(context):
+    assert isinstance(context.exception, BankAccountAlreadyExists)
