897e007be6d0d50b2ab27467ff69ea20dae74891
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-07-04 14:40:42 +0300

    test: implement feature tests for user services

--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -1,31 +1,46 @@
 from behave import given, when, then
 
+from exceptions.user_exceptions import InvalidCredentials, UserNotFound
+from models.user import User
+from utils import password
+
 @given("the user is registered")
 def step_impl(context):
-    context.user = {
-        "username": "user_123",
-        "email": "password@123",
-    }
+    context.user = User(
+        username="user_123",
+        password="password@12",
+        email="user1@example.com",
+        first_name="fuser",
+        last_name="luser",
+        age=20,
+        img_url="https://example.com",
+        bank_account=[]
+    )
+    context.user_service.register(context.user)
 
 @when("the user enters the valid credentials")
 def step_impl(context):
-    context.result = True
+    context.result = context.user_service.login("user_123","password@12")
 
 @then("the login should be successfully")
 def step_impl(context):
-    assert context.result is True
+    assert context.result is not None
+    assert context.result.username == "user_123"
 
 
 @given("the user is trying to login")
 def step_impl(context):
-    context.user = {
-        "username": "",
-        "email": "password@123",
-    }
+    context.user = context.user = User(
+        username = "user_123",
+        password = "password123"
+    )
 @when("the user enters the invalid credentials")
 def step_impl(context):
-    context.result = False
+    try:
+        context.result = context.user_service.login(context.user.username, context.user.password)
+    except (UserNotFound, InvalidCredentials) as e:
+        context.exception = e
 
 @then("the login should not be successfully")
 def step_impl(context):
-    assert context.result is False
+    assert isinstance(context.exception, UserNotFound)
--- features/steps/register_steps.py
+++ features/steps/register_steps.py
@@ -1,40 +1,47 @@
 from behave import given, when, then
+from models.user import User
 
 @given("the user")
 def step_impl(context):
-    context.user = {
-        "username" : "user_123",
-        "password" : "password@12",
-        "email" : "user@example.com",
-        "first_name" : "fuser",
-        "last_name" : "luser",
-        "age" : 20,
-        "image" : "https://example.com"
-    }
+    context.user = User(
+    username="user_13",
+    password="password@12",
+    email="user1@example.com",
+    first_name="fuser",
+    last_name="luser",
+    age=20,
+    img_url="https://example.com",
+    bank_account=[]
+)
+
 
 @when("the user enters the valid values")
 def step_impl(context):
-    context.result = True
+    context.result = context.user_service.register(context.user)
+
 
 @then("the register should be successfully")
 def step_impl(context):
-    assert context.result is True
+    assert context.result is not None
+    assert context.result.username == "user_13"
+    assert context.result.email == "user1@example.com"
 
 @given("the user is trying to register")
 def step_impl(context):
-    context.user = {
-        "username": "",
-        "password": "password@12",
-        "email": "",
-        "first_name": "fuser",
-        "last_name": "luser",
-        "age": 20,
-        "image": "https://example.com"
-    }
+    context.user = User(
+    username="user_13",
+    password="password@12",
+    email="user1@example.com",
+    first_name="fuser",
+    last_name="luser",
+    age=20,
+    img_url="https://example.com",
+    bank_account=[]
+    )
 @when("the users enters the invalid values")
 def step_impl(context):
-    context.result = False
+    context.result = context.result = context.user_service.register(context.user)
 
 @then("the register should not be happened")
 def step_impl(context):
-    assert context.result is False
+    assert context.result is not None
