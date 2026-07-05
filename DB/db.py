51aa873bdc48f19b7e8378a0e5643afef31957de
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-07-05 16:24:24 +0300

    init

--- DB/db.py
+++ DB/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
+
 DATABASE_URL = "sqlite:///bank.db"
 
 engine = create_engine(DATABASE_URL , echo=True) # connect app with db
