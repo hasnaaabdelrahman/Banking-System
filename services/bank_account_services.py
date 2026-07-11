9317fc2313b450ca32854320d2836f603b1ecbd3
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-07-11 10:42:13 +0300

    refactor: use bank account schema

--- services/bank_account_services.py
+++ services/bank_account_services.py
@@ -1,14 +1,25 @@
 from models.bank_account import BankAccount
 from exceptions.bank_account_exceptions import BankAccountNotFound , BankAccountAlreadyExists
+from schemas.bank_account_schema import BankAccountSchema
 
 
 class BankAccountService:
     def __init__(self , bank_repository):
         self.bank_repository = bank_repository
 
-    def create_bank_account(self , bank_account:BankAccount) -> BankAccount:
-        if self.bank_repository.get_by_id(bank_account.id) or self.bank_repository.get_by_user_id(bank_account.user_id):
+    def create_bank_account(self , bank_account_data:BankAccountSchema) -> BankAccount:
+
+        if self.bank_repository.get_by_user_id(bank_account_data.user_id):
             raise BankAccountAlreadyExists("Bank account already exists")
+
+        bank_account =BankAccount(
+            account_number = bank_account_data.account_number,
+            account_type= bank_account_data.account_type,
+            is_active= bank_account_data.is_active,
+            balance= bank_account_data.balance,
+            user_id=bank_account_data.user_id
+        )
+
         self.bank_repository.create(bank_account)
         return bank_account
 
