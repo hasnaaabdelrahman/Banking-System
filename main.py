615054bf69251654341b0b141e371e21add42681
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-07-16 16:41:29 +0300

    refactor: add prefix urls

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from controllers.bank_account_controller import bank_bp
 from controllers.transaction_controller import transaction_bp
 app = Flask(__name__)
 
-app.register_blueprint(user_bp)
-app.register_blueprint(bank_bp)
-app.register_blueprint(transaction_bp)
+app.register_blueprint(user_bp, url_prefix='/users')
+app.register_blueprint(bank_bp, url_prefix='/accounts')
+app.register_blueprint(transaction_bp, url_prefix='/transactions')
 
 if __name__ == '__main__':
     app.run(debug=True)
