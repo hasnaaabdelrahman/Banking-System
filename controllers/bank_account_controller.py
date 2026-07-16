42181392dfe0de3af91885e179ab23efa41294b6
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-07-16 18:27:55 +0300

    fix: create a new account endpoint

--- controllers/bank_account_controller.py
+++ controllers/bank_account_controller.py
@@ -1,5 +1,7 @@
 from flask import Flask, request , Blueprint, jsonify
 
+from exceptions.bank_account_exceptions import BankAccountNotFound, BankAccountAlreadyExists
+from schemas.bank_account_schema import BankAccountSchema
 from services.bank_account_services import BankAccountService
 from repositories.bank_account_repository import BankAccountRepository
 from database.db import session
@@ -11,70 +13,79 @@ bank_account_service = BankAccountService(bank_account_repository)
 
 @bank_bp.route('/bank_account', methods=['POST'])
 def create_bank_account():
-    data = request.json
-    bank_account = bank_account_service.create_bank_account(data)
-    return jsonify({
-        "message": "Bank account created",
-        "bank account": {
-            "id": bank_account.id,
-            "account_number": bank_account.account_number,
-            "account_type": bank_account.account_type,
-            "is_active": bank_account.is_active,
-            "balance": bank_account.balance,
-            "user_id": bank_account.user_id,
-        }
-    }), 201
+    try:
+        data = request.json
+        account_data = BankAccountSchema(**data)
+        bank_account = bank_account_service.create_bank_account(account_data)
+        return jsonify({
+            "message": "Bank account created",
+            "bank account": {
+                "id": bank_account.id,
+                "account_number": bank_account.account_number,
+                "account_type": bank_account.account_type.value,
+                "is_active": bank_account.is_active,
+                "balance": bank_account.balance,
+                "user_id": bank_account.user_id,
+            }
+        }), 201
+    except BankAccountAlreadyExists as e:
+        return jsonify({"message": str(e)}), 409
 
 @bank_bp.route('/bank_account/<string:id>', methods=['GET'])
 def get_bank_account(id:str):
-    bank_account = bank_account_service.get_by_id(id)
-    if not bank_account:
-        return jsonify({"message": "Bank account not found"}), 404
-    return jsonify({
-        "bank account": {
-            "id": bank_account.id,
-            "account_number": bank_account.account_number,
-            "account_type": bank_account.account_type,
-            "is_active": bank_account.is_active,
-            "balance": bank_account.balance,
-        }
-    }), 200
+    try:
+        bank_account = bank_account_service.get_by_id(id)
+        return jsonify({
+            "bank account": {
+                "id": bank_account.id,
+                "account_number": bank_account.account_number,
+                "account_type": bank_account.account_type.value,
+                "is_active": bank_account.is_active,
+                "balance": bank_account.balance,
+            }
+        }), 200
+    except BankAccountNotFound as e:
+        return jsonify({"message": str(e)}), 404
 
 @bank_bp.route('/user/<string:user_id>/bank_account', methods=['GET'])
 def get_user_bank_account(user_id:str):
-    bank_account = bank_account_service.get_by_user_id(user_id)
-    if not bank_account:
-        return jsonify({"message": "Bank account not found"}), 404
-    return jsonify({
-        "bank account": {
-            "account_number": bank_account.account_number,
-            "account_type": bank_account.account_type,
-            "is_active": bank_account.is_active,
-            "balance": bank_account.balance,
-            "user_id": bank_account.user_id,
-        }
-    }), 200
+    try:
+        bank_account = bank_account_service.get_by_user_id(user_id)
+        return jsonify({
+            "bank account": {
+                "id": bank_account.id,
+                "account_number": bank_account.account_number,
+                "account_type": bank_account.account_type.value,
+                "is_active": bank_account.is_active,
+                "balance": bank_account.balance,
+                "user_id": bank_account.user_id,
+            }
+        }), 200
+    except BankAccountNotFound as e:
+        return jsonify({"message": str(e)}), 404
 
 @bank_bp.route('/bank_account/<string:id>', methods=['PUT'])
 def update_bank_account(id:str):
-    bank_account = bank_account_service.update_by_id(id)
-    if not bank_account:
-        return jsonify({"message": "Bank account not found"}), 404
-    return jsonify({
-        "message": "Bank account updated",
-        "bank account": {
-            "id": bank_account.id,
-            "account_number": bank_account.account_number,
-            "account_type": bank_account.account_type,
-            "is_active": bank_account.is_active,
-            "balance": bank_account.balance,
-            "user_id": bank_account.user_id,
-        }
-    }), 200
+    try:
+        bank_account = bank_account_service.update_by_id(id)
+        return jsonify({
+            "message": "Bank account updated",
+            "bank account": {
+                "id": bank_account.id,
+                "account_number": bank_account.account_number,
+                "account_type": bank_account.account_type.value,
+                "is_active": bank_account.is_active,
+                "balance": bank_account.balance,
+                "user_id": bank_account.user_id,
+            }
+        }), 200
+    except BankAccountNotFound as e:
+        return jsonify({"message": str(e)}), 404
 
 @bank_bp.route('/bank_account/<string:id>', methods=['DELETE'])
 def delete_bank_account(id:str):
-    bank_data= bank_account_service.delete_by_id(id)
-    if not bank_data:
-        return jsonify({"message": "Bank account not found"}), 404
-    return jsonify({"message": "Bank account deleted successfully"}), 204
+    try:
+        bank_account_service.delete_by_id(id)
+        return jsonify({"message": "Bank account deleted successfully"}), 204
+    except BankAccountNotFound as e:
+        return jsonify({"message": str(e)}), 404
--- services/bank_account_services.py
+++ services/bank_account_services.py
@@ -8,18 +8,15 @@ class BankAccountService:
         self.bank_repository = bank_repository
 
     def create_bank_account(self , bank_account_data:BankAccountSchema) -> BankAccount:
-
-        if self.bank_repository.get_by_user_id(bank_account_data.user_id):
+        account = self.bank_repository.get_by_user_id (bank_account_data.user_id)
+        if account:
             raise BankAccountAlreadyExists("Bank account already exists")
-
         bank_account =BankAccount(
-            account_number = bank_account_data.account_number,
             account_type= bank_account_data.account_type,
             is_active= bank_account_data.is_active,
             balance= bank_account_data.balance,
             user_id=bank_account_data.user_id
         )
-
         self.bank_repository.create(bank_account)
         return bank_account
 
