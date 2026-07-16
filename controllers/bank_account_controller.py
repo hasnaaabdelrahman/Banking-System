c3ab253ed2a11f1ee812e37f5899cf0cd59f1d1c
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-07-16 16:41:08 +0300

    fix: string converter in id

--- controllers/bank_account_controller.py
+++ controllers/bank_account_controller.py
@@ -13,24 +13,68 @@ bank_account_service = BankAccountService(bank_account_repository)
 def create_bank_account():
     data = request.json
     bank_account = bank_account_service.create_bank_account(data)
-    return jsonify(bank_account), 201
+    return jsonify({
+        "message": "Bank account created",
+        "bank account": {
+            "id": bank_account.id,
+            "account_number": bank_account.account_number,
+            "account_type": bank_account.account_type,
+            "is_active": bank_account.is_active,
+            "balance": bank_account.balance,
+            "user_id": bank_account.user_id,
+        }
+    }), 201
 
-@bank_bp.route('/bank_account/<str:id>', methods=['GET'])
+@bank_bp.route('/bank_account/<string:id>', methods=['GET'])
 def get_bank_account(id:str):
     bank_account = bank_account_service.get_by_id(id)
-    return jsonify(bank_account), 200
+    if not bank_account:
+        return jsonify({"message": "Bank account not found"}), 404
+    return jsonify({
+        "bank account": {
+            "id": bank_account.id,
+            "account_number": bank_account.account_number,
+            "account_type": bank_account.account_type,
+            "is_active": bank_account.is_active,
+            "balance": bank_account.balance,
+        }
+    }), 200
 
-@bank_bp.get('/user/<str:user_id>/bank_account', methods=['GET'])
+@bank_bp.route('/user/<string:user_id>/bank_account', methods=['GET'])
 def get_user_bank_account(user_id:str):
     bank_account = bank_account_service.get_by_user_id(user_id)
-    return jsonify(bank_account), 200
+    if not bank_account:
+        return jsonify({"message": "Bank account not found"}), 404
+    return jsonify({
+        "bank account": {
+            "account_number": bank_account.account_number,
+            "account_type": bank_account.account_type,
+            "is_active": bank_account.is_active,
+            "balance": bank_account.balance,
+            "user_id": bank_account.user_id,
+        }
+    }), 200
 
-@bank_bp.put('/bank_account/<str:id>', methods=['PUT'])
+@bank_bp.route('/bank_account/<string:id>', methods=['PUT'])
 def update_bank_account(id:str):
     bank_account = bank_account_service.update_by_id(id)
-    return jsonify(bank_account), 200
+    if not bank_account:
+        return jsonify({"message": "Bank account not found"}), 404
+    return jsonify({
+        "message": "Bank account updated",
+        "bank account": {
+            "id": bank_account.id,
+            "account_number": bank_account.account_number,
+            "account_type": bank_account.account_type,
+            "is_active": bank_account.is_active,
+            "balance": bank_account.balance,
+            "user_id": bank_account.user_id,
+        }
+    }), 200
 
-@bank_bp.delete('/bank_account/<str:id>', methods=['DELETE'])
+@bank_bp.route('/bank_account/<string:id>', methods=['DELETE'])
 def delete_bank_account(id:str):
-    bank_account_service.delete_by_id(id)
-    return '', 204
+    bank_data= bank_account_service.delete_by_id(id)
+    if not bank_data:
+        return jsonify({"message": "Bank account not found"}), 404
+    return jsonify({"message": "Bank account deleted successfully"}), 204
--- controllers/transaction_controller.py
+++ controllers/transaction_controller.py
@@ -12,32 +12,81 @@ transaction_bp = Blueprint('transaction', __name__)
 @transaction_bp.route('/transaction', methods=['POST'])
 def create_transaction():
     data = request.json
-    transaction = transaction_service.create_transaction(data)
-    return jsonify(transaction), 201
+    transaction_data = transaction_service.create_transaction(data)
+    return jsonify({
+        "message": "Transaction created successfully",
+        "transaction": {
+            "id": transaction_data.id,
+            "account_id": transaction_data.account_id,
+            "amount": transaction_data.amount,
+            "date": transaction_data.date,
+            "transaction_type": transaction_data.transaction_type
+        }
+    }), 201
 
-@transaction_bp.route('/transaction/<str:id>', methods=['GET'])
+@transaction_bp.route('/transaction/<string:id>', methods=['GET'])
 def get_transaction(id:str):
-    transaction = transaction_service.get_transaction_by_id(id)
-    return jsonify(transaction), 200
+    transaction_data = transaction_service.get_transaction_by_id(id)
+    if not transaction_data:
+        return jsonify({"message": "Transaction not found"}), 404
+    return jsonify({
+        "id": transaction_data.id,
+        "account_id": transaction_data.account_id,
+        "amount": transaction_data.amount,
+        "date": transaction_data.date,
+        "transaction_type": transaction_data.transaction_type
+    }), 200
 
 @transaction_bp.route('/transactions', methods=['GET'])
 def get_all_transactions():
     transactions = transaction_service.get_all_transactions()
-    return jsonify(transactions), 200
+    return jsonify({
+        "transactions":[ {
+                "id": transaction_data.id,
+                "account_id": transaction_data.account_id,
+                "amount": transaction_data.amount,
+                "date": transaction_data.date,
+                "transaction_type": transaction_data.transaction_type
+        }] for transaction_data in transactions
+    }), 200
 
-@transaction_bp.route('/transaction/<str:id>', methods=['GET'])
+@transaction_bp.route('/transaction/<string:id>', methods=['GET'])
 def get_transaction_by_account_id(id:str):
-    transaction = transaction_service.get_transaction_by_account_id(id)
-    return jsonify(transaction), 200
+    transactions = transaction_service.get_transaction_by_account_id(id)
+    if not transactions:
+        return jsonify({"message": "Transaction not found"}), 404
+
+    return jsonify({
+        "transaction": [{
+        "id": transaction_data.id,
+        "account_id": transaction_data.account_id,
+        "amount": transaction_data.amount,
+        "date": transaction_data.date,
+        "transaction_type": transaction_data.transaction_type
+        }] for transaction_data in transactions
+    }), 200
 
 @transaction_bp.route('/transaction', methods=['PUT'])
 def update_transaction():
     data = request.json
-    transaction = transaction_service.update_transaction(data)
-    return jsonify(transaction), 200
+    transaction_data = transaction_service.update_transaction(data)
+    if not transaction_data:
+        return jsonify({"message": "Transaction not found"}), 404
+    return jsonify({
+        "message": "Transaction updated successfully",
+        "transaction": {
+            "id": transaction_data.id,
+            "account_id": transaction_data.account_id,
+            "amount": transaction_data.amount,
+            "date": transaction_data.date,
+            "transaction_type": transaction_data.transaction_type
+        }
+    }), 200
 
 @transaction_bp.route('/transaction', methods=['DELETE'])
 def delete_transaction():
     data = request.json
-    transaction_service.delete_transaction(data)
-    return '' , 204
+    transaction_data = transaction_service.delete_transaction(data)
+    if not transaction_data:
+        return jsonify({"message": "Transaction not found"}), 404
+    return jsonify({"message": "Transaction deleted successfully"}) , 204
